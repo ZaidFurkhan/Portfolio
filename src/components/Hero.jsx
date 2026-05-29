b681bb3f21b7b4b49307dc2f652e184cf550444d
ZaidFurkhan <zaidfurkhan2@gmail.com>
2026-05-29 10:55:16 +0530

    resume updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 export default function Hero({ className }) {
     return (
-        <section id="home" className={`relative overflow-hidden w-full h-[100vh] bg-gradient-to-r from-gray-300 via-gray-200 to-gray-50 text-black flex items-center md:items-end pb-20 justify-start animate-in slide-in-from-bottom duration-800 px-8 gap-10 pt-24 md:pt-32 ${className}`}  >
+        <section id="home" className={`relative overflow-hidden w-full h-[100vh] bg-gradient-to-r from-gray-300 via-gray-200 to-gray-50 text-black flex items-end pb-20 justify-start animate-in slide-in-from-bottom duration-800 px-8 gap-10 pb-50 md:pb-25 md:pt-32 ${className}`}  >
             <div className="flex flex-col w-full mt-4 space-y-2 relative animate-in slide-in-from-bottom duration-800">
                 <h2 className="font-extrabold text-xl md:text-4xl mb-2">Hello,</h2>
                 <h1 className="font-extrabold max-w-5xl text-3xl md:text-7xl lg:text-8xl">I'm <span className="bg-gradient-to-r from-orange-600/90 to-orange-400/80 text-transparent bg-clip-text typing ">Zaid Furkhan</span></h1>
@@ -13,7 +13,7 @@ export default function Hero({ className }) {
             </div>
 
             {/* Decorative curved lines */}
-            <div className="absolute bottom-0 right-0 pointer-events-none opacity-45">
+            <div className="absolute bottom-0 right-0 pointer-events-none -z-10 opacity-45">
                 <svg width="600" height="600" viewBox="0 0 600 600" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M600 100C400 100 200 300 200 600" stroke="#94a3b8" strokeWidth="0.75" />
                     <path d="M600 200C450 200 300 350 300 600" stroke="#94a3b8" strokeWidth="0.75" />
